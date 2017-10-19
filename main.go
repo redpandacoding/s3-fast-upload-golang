dc135313c26f6bd545415dbb4ed95754389faeab
Whit Morriss <d.w.morriss@gmail.com>
2017-10-19 13:01:13 -0700

    chained credentials

--- main.go
+++ main.go
@@ -2,21 +2,44 @@ package main
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/golang/glog"
+	"github.com/op/go-logging"
+	"gopkg.in/alecthomas/kingpin.v2"
+	//"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"os"
-	"path"
+	//"path"
 	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
 	//  "github.com/aws/aws-sdk-go/aws/awserr"
+	//"github.com/golang/glog"
+)
+
+
+var (
+	app         = kingpin.New("chat", "A command-line chat application. see: https://github.com/whitmo/s3-fast-upload-golang")
+	bucket      = app.Flag("bucket", "s3 bucket to upload to").Required().String()
+	subfolder   = app.Flag("subfolder", "subfolder in s3 bucket, can be blank").String()
+	num_workers = app.Flag("workers", "number of upload workers to use").Default("100").Int()
+	region      = app.Flag("region", "aws region").Default("us-west-1").String()
+	acl         = app.Flag("acl", "s3 upload acl - use either private or public").Default("private").String()
+	sourceDir   = app.Arg("sourcedir", "source directory").Default("./").String()
+	//destDir     = app.Arg("destdir", "dest dir for uploaded files (on local box)").String()
+
+	verbose = app.Flag("verbose", "Be verbose").Bool()
+	ec2_iam = app.Flag("ec2-role", "Running with instance iam role").Bool()
+	log = logging.MustGetLogger("example")
+	format = logging.MustStringFormatter(
+		`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+	)
 )
 
 // definition of a worker instance
@@ -28,7 +51,7 @@ type Worker struct {
 	File_channel chan string     // the channel to get file names from (upload todo list)
 	Wg           *sync.WaitGroup // wait group - to signal when worker is finished
 	SourceDir    string          // where source files are to be uploaded
-	DestDir      string          // where to move uploaded files to (on local box)
+	//DestDir      string          // where to move uploaded files to (on local box)
 	Id           int             // worker id number for debugging
 }
 
@@ -115,16 +138,17 @@ func (worker *Worker) doUploads() {
 		worker.println("File to upload: " + file)
 		response, err := worker.upload(file)
 		if err != nil {
-			worker.println("error uploading" + file + ": " + response + " " + err.Error())
-		} else {
-			worker.println(response)
-			// make destination directory if needed
-			filename := path.Base(file)
-			directory := strings.Replace(file, "/"+filename, "", 1)
-			os.MkdirAll(worker.DestDir+directory, 0775)
-			// move file
-			os.Rename(worker.SourceDir+file, worker.DestDir+file)
+			log.Error("error uploading" + file + ": " + response + " " + err.Error())
 		}
+		// else {
+		// 	log.Info(response)
+		// 	// make destination directory if needed
+		// 	filename := path.Base(file)
+		// 	directory := strings.Replace(file, "/"+filename, "", 1)
+		// 	os.MkdirAll(worker.DestDir+directory, 0775)
+		// 	// move file
+		// 	os.Rename(worker.SourceDir+file, worker.DestDir+file)
+		// }
 	}
 	worker.println("doUploads() finished")
 }
@@ -132,57 +156,73 @@ func (worker *Worker) doUploads() {
 // function to print out messages prefixed with worker-[id]
 func (worker *Worker) println(message string) {
 	msg := fmt.Sprintln("Worker-" + strconv.Itoa(worker.Id) + ": " + message)
-	glog.Info(msg)
+	log.Info(msg)
 }
 
 func main() {
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+	logging.SetBackend(backend1Leveled)
 
-	bucketFlag := flag.String("bucket", "my-s3-bucket", "s3 bucket to upload to")
-	subfolderFlag := flag.String("subfolder", "", "subfolder in s3 bucket, can be blank")
-	num_workersFlag := flag.Int("workers", 100, "number of upload workers to use")
-	regionFlag := flag.String("region", "eu-west-1", "aws region")
-	aclFlag := flag.String("acl", "private", "s3 upload acl - use either private or public")
-	sourceDirFlag := flag.String("sourcedir", "files/", "source directory")
-	destDirFlag := flag.String("destdir", "files-uploaded/", "dest dir for uploaded files (on local box)")
-
-	flag.Parse()
-
-	bucket := *bucketFlag
-	subfolder := *subfolderFlag
-	num_workers := *num_workersFlag
-	region := *regionFlag
-	acl := *aclFlag
-	sourceDir := *sourceDirFlag
-	destDir := *destDirFlag
-
-	fmt.Println("Using options:")
-	fmt.Println("  bucket:", bucket)
-	fmt.Println("  subfolder:", subfolder)
-	fmt.Println("  num_workers:", num_workers)
-	fmt.Println("  region:", region)
-	fmt.Println("  acl:", acl)
-	fmt.Println("  sourceDir:", sourceDir)
-	fmt.Println("  destDir:", destDir)
+	if *verbose == true {
+		fmt.Println("Using options:")
+		fmt.Println("  bucket:", bucket)
+		fmt.Println("  subfolder:", subfolder)
+		fmt.Println("  num_workers:", num_workers)
+		fmt.Println("  region:", region)
+		fmt.Println("  acl:", acl)
+		fmt.Println("  sourceDir:", sourceDir)
+		//fmt.Println("  destDir:", destDir)
+	}
 
 	var wg sync.WaitGroup
-	wg.Add(num_workers + 1) // add 1 to account for the get_file_list thread!
+	wg.Add(*num_workers + 1) // add 1 to account for the get_file_list thread!
 
 	// file channel and thread to get the files
 	file_channel := make(chan string, 0)
-	go get_file_list(sourceDir, file_channel, num_workers, &wg)
+	go get_file_list(*sourceDir, file_channel, *num_workers, &wg)
 
 	// set up s3 credentials from environment variables
 	// these are shared to every worker
-	creds := credentials.NewEnvCredentials()
 
-	fmt.Println("Starting " + strconv.Itoa(num_workers) + " workers...")
+	base_sess := session.Must(session.NewSession(aws.NewConfig()))
+	creds := credentials.NewChainCredentials(
+		[]credentials.Provider{
+			&credentials.EnvProvider{},
+			&ec2rolecreds.EC2RoleProvider{
+				Client: ec2metadata.New(base_sess),
+			},
+		})
+
+	sess := session.New(&aws.Config{
+		Region: aws.String(*region),
+		Credentials: creds,
+		LogLevel: aws.LogLevel(1)})
+
+	// if *ec2_iam != true {
+	// 	creds := credentials.NewEnvCredentials()
+	// } else {
+	// 	creds := credentials.NewCredentials(&ec2rolecreds.EC2RoleProvider{})
+	// }
+
+	log.Info(fmt.Sprintln("Starting " + strconv.Itoa(*num_workers) + " workers..."))
 
 	// create the desired number of workers
-	for i := 1; i <= num_workers; i++ {
+	for i := 1; i <= *num_workers; i++ {
 		// make a new worker
-		sess := session.New(&aws.Config{Region: aws.String(region), Credentials: creds, LogLevel: aws.LogLevel(1)})
 		svc := s3.New(sess)
-		worker := &Worker{Acl: acl, Bucket: bucket, Subfolder: subfolder, Svc: svc, File_channel: file_channel, Wg: &wg, SourceDir: sourceDir, DestDir: destDir, Id: i}
+		worker := &Worker{
+			Acl:          *acl,
+			Bucket:       *bucket,
+			Subfolder:    *subfolder,
+			Svc:          svc,
+			File_channel: file_channel,
+			Wg:           &wg,
+			SourceDir:    *sourceDir,
+			//DestDir:      *destDir, Id: i,
+		}
 		go worker.doUploads()
 	}
 
